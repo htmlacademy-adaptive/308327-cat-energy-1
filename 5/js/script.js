bb11ba68a53c1d293811dcdbef69dd49b5130d26
Keks <keks@htmlacademy.ru>
2023-11-18 15:11:25 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/script.js
@@ -0,0 +1,21 @@
+let mainNav = document.querySelector('.main-header__navigation');
+let menuButton = document.querySelector('.main-header__menu-btn');
+let mainNavList = document.querySelector('.main-header__navigation-list');
+
+mainNav.classList.remove('main-header__navigation--nojs');
+mainNavList.classList.remove('main-header__navigation-list--nojs');
+menuButton.classList.remove('main-header__menu-btn--nojs');
+
+menuButton.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-header__navigation--closed')) {
+    mainNav.classList.remove('main-header__navigation--closed');
+    mainNav.classList.add('main-header__navigation--opened');
+    menuButton.classList.remove('main-header__menu-btn--opened');
+    menuButton.classList.add('main-header__menu-btn--closed');
+  } else {
+    mainNav.classList.add('main-header__navigation--closed');
+    mainNav.classList.remove('main-header__navigation--opened');
+    menuButton.classList.add('main-header__menu-btn--opened');
+    menuButton.classList.remove('main-header__menu-btn--closed');
+  }
+});
